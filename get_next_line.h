ee4d2617318f2ead47e19929d08b4159c8c395d3
lilbur <43801400+lilbur@users.noreply.github.com>
2019-11-14 21:18:55 -0800

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -1,7 +1,7 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # define BUFFSIZE 1024
-# define MAX_FD 2147483647
+# define MAX_FD 256
 # include <unistd.h>
 
 int get_next_line(const int fd, char **line)
