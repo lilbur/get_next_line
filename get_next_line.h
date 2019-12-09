8362420e04611ae2e39b6653dfee87b762877c66
lilbur <43801400+lilbur@users.noreply.github.com>
2019-12-08 20:44:55 -0800

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -1,15 +1,9 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFFSIZE 1024
+# define BUFF_SIZE 500
 # define MAX_FD 256
-# include <unistd.h>
+# include "libft/libft.h"
 
-typedef struct  s_list
-{
-  void          *content
-  struct s_list  *next;
-}               t_queue;
+int get_next_line(const int fd, char **line);
 
-int get_next_line(const int fd, char **line)
-  
 #endif
