cdd78fec8fb7666e9d56608c7c1c52a2967feae6
lilbur <43801400+lilbur@users.noreply.github.com>
2019-12-08 20:45:43 -0800

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -1,20 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmacias <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/06 20:01:59 by tmacias           #+#    #+#             */
+/*   Updated: 2019/12/08 20:45:17 by tmacias          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-int get_next_line(const int fd, char **line)
+int	get_next_line(const int fd, char **line)
 {
-  ssize_t     bytes;
-  char        *buffer;
-  static char *ln[MAX_FD];
-  
-  if (**line != NULL || fd > MAX_FD)
-    return (-1);
-  buffer = NULL;
-  while((bytes = read(fd, &buffer, BUFF_SIZE) > 0)
-        {
-          ln[fd] = ft_strjoin(ln[fd], buffer);
-          if (strchr(buffer, '\n'))
-          {
-            *line = buffer;
-          }
- }
-    
+	static char	*ln[MAX_FD] = { NULL };
+	char		buf[BUFF_SIZE + 1];
+	int			end;
+	int			res;
+	char		*tmp;
+
+	tmp = NULL;
+	if (fd < 0 || line == NULL)
+		return (-1);
+	ft_memset(buf, 0, BUFF_SIZE + 1);
+	while ((res = read(fd, buf, BUFF_SIZE)) > 0)
+	{
+		if (ln[fd] != NULL)
+			ln[fd] = ft_strjoin(ln[fd], buf);
+		else
+			ln[fd] = ft_strdup(buf);
+		if (ft_strchr(ln[fd], '\n'))
+			break ;
+		ft_memset(buf, 0, BUFF_SIZE + 1);
+	}
+	if (res == -1)
+		return (-1);
+	if (!res && !ln[fd])
+		return (0);
+	end = ft_strlchr(ln[fd], '\n');
+	if (end > -1)
+	{
+		*line = ft_strsub(ln[fd], 0, end);
+		if (!(end + 1 == (int)ft_strlen(ln[fd])))
+			tmp = ft_strdup(ft_strchr(ln[fd], '\n') + 1);
+		free(ln[fd]);
+		ln[fd] = tmp;
+	}
+	else if (res == 0)
+	{
+		*line = ln[fd];
+		ln[fd] = NULL;
+	}
+	return (1);
+}
